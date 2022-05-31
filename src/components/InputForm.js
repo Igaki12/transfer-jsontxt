02fb7125b8c83bc2a4e11decc7e19fa459a0c2c1
Igaki12 <91039412+Igaki12@users.noreply.github.com>
2022-05-31 11:01:22 +0900

    質問本文の区別機能を確立

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,24 +1,92 @@
 import { Text, Textarea } from '@chakra-ui/react'
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 export const InputForm = ({ inputEl }) => {
   const [inputInfo, setInputInfo] = useState({
     row: 0,
     length: 0,
   })
-  const CountInputTxt = () => {
-    setInputInfo({ length: inputEl.current.value.length })
+  const [preview, setPreview] = useState([])
+
+  const countInputTxt = () => {
+    console.log(inputEl.current.value)
+    // let spaceCut = inputEl.current.value.split(' ').join('')
+    // preview = [inputEl.current.value.split(/\n/g)]
+    let sentences = inputEl.current.value
+      .split(/\n\s+/g)
+      .filter(
+        (row) =>
+          row !== '' &&
+          row !== '  ' &&
+          row.indexOf(
+            'このフォームを送信すると、 所有者に名前とメールアドレスが表示されます。',
+          ) === -1 &&
+          row.indexOf('送信') === -1,
+      )
+    let questionNum = 0
+    // let choiceNum = 0
+    sentences.forEach((sentence) => {
+      console.log('チェック開始：' + sentence)
+      if (sentence.match(/^[0-9]*\./)) {
+        questionNum++
+        // choiceNum = 0
+        if (sentence.indexOf('questionSentence:') === -1) {
+          sentence = `{\ndetailInfo:'(${questionNum})',\nquestionSentence:'${
+            sentence.split(/^[0-9]*\./)[1]
+          }'
+          choices:[`
+        }
+        console.log('[0-9].を検出：' + sentence)
+        console.log(sentence.split(/^[0-9]*\./)[0])
+      } else {
+        // choiceNum++
+        sentence = `choices:[`
+      }
+    })
+    setPreview(
+      sentences,
+      // inputEl.current.value
+      //   .split(/\n\s+/g)
+      //   .filter(
+      //     (row) =>
+      //       row !== '' &&
+      //       row !== '  ' &&
+      //       row.indexOf(
+      //         'このフォームを送信すると、 所有者に名前とメールアドレスが表示されます。',
+      //       ) === -1 &&
+      //       row.indexOf('送信') === -1,
+      //   ),
+    )
+    console.log(preview)
+
+    setInputInfo({
+      row: inputEl.current.value.split(/\n/g).length,
+      length: inputEl.current.value.replace(/\s+/g, '').length,
+    })
   }
   return (
     <>
-      <Text mb="8px" size={'sm'}>
-        Value:{inputEl.current.value}
-      </Text>
+      {preview ? (
+        <>
+          {preview.map((value, i) => (
+            <Text mb="8px" size={'sm'} key={i}>
+              {value}
+            </Text>
+          ))}
+        </>
+      ) : (
+        <Text mb="8px" size={'sm'}>
+          'ここにプレビューが表示されます'
+        </Text>
+      )}
       <Textarea
         ref={inputEl}
-        onChange={CountInputTxt}
+        onChange={countInputTxt}
+        onBlur={countInputTxt}
         placeholder="ここにスキャンした文字列を貼り付け"
       ></Textarea>
-      <Text>{inputInfo.length}文字</Text>
+      <Text>
+        {inputInfo.row}行{inputInfo.length}文字
+      </Text>
     </>
   )
 }
